e0fd67627de9a27e191b2a9942e9481372486753
Carole <cjpillette@hotmail.com>
2017-04-20 16:39:06 +1000

    implemented forum discussion and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
+
   resources :forum_posts
   resources :genres
   resources :forums
   resources :songs
   resources :playlists
   resources :users
+
+  root 'welcome#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
